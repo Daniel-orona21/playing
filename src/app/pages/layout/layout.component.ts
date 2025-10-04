ad41931830eef79325f75c95b56f3886f17d7299
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-10-04 01:43:23 -0600

    layout - header

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  public pause = false;
+  value = 50;
+  selectedNavItem: string | 'music' = 'music';
 
+
+  play() {
+    this.pause = !this.pause;
+  }
+
+  updateValue(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const percent = ((+input.value - +input.min) / (+input.max - +input.min)) * 100;
+    input.style.setProperty('--value', percent + '%');
+  }
+
+  selectNavItem(item: string) {
+    this.selectedNavItem = item;
+  }
 }
